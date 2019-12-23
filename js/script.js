d853d2f9ef0e1f05f427f80a214b0c0cbd889193
Pham Nguyen Truong Giang <giangpham.1751996@gmail.com>
2019-12-23 14:41:36 +0700

    .

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// $(function() {
+//     $(window).scroll(function() {
+//         $(".a.nav-link.buy").click(function() {
+//             $("body").animate({ scrollTop: $("#oderInfo").offset().top }, 1000);
+//             return false;
+//         });
+//     });
+
+//     $(window).scroll(function() {
+//         $("button.btn.align-self-end.main").click(function() {
+//             $("body").animate({ scrollTop: $("#oderInfo").offset().top }, 1000);
+//             return false;
+//         });
+//     });
+
+// });
